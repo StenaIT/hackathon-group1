58ba1b4aaafc55fa63675f138ea2ab0701b9c4f5
Anton Karlsson <anton.karlsson@stena.com>
2016-09-15 12:39:38 +0200

    added simple parse of the letter

--- js/script.js
+++ js/script.js
@@ -1,11 +1,12 @@
 
-//var obj = jQuery.parseJSON( '{ "name": "John" }' );
-//alert( obj.name === "John" );
-
-
 function getText(){
-    var url = "http://verksamtproxy.hackastack.de18292d.svc.dockerapp.io";
+    
+    var url = "http://verksamtproxy.hackastack.de18292d.svc.dockerapp.io/";
+    
     $.getJSON(url, function(data){
-        alert(data);
+        
+        var a = data[0]
+        alert(a.Letter);
     });
+
 }
