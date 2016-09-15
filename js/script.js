7b06c5d733dbda079b355e0826f6d105538da1ed
Anton Karlsson <anton.karlsson@stena.com>
2016-09-15 13:51:34 +0200

    Added a nice algorithm to translate wired words.

--- js/script.js
+++ js/script.js
@@ -1,12 +1,35 @@
 
 function getText(){
-    
+
+ $(document).ready(function() {
     var url = "http://verksamtproxy.hackastack.de18292d.svc.dockerapp.io/";
     
-    $.getJSON(url, function(data){
-        
-        var a = data[0]
-        alert(a.Letter);
-    });
+    var letters = ['A','B','C','D'];
 
+    $.getJSON(url, function(data){
+        var first = getFirstLetter();
+        var input = $('#term').val();
+        var indexOf = letters.indexOf(first) 
+        var allWords = data[indexOf].words;
+
+        var description = "";
+        allWords.forEach(function(element) {
+            if(input == element.word){
+               description = element.description;     
+            }
+        }, this);
+
+        alert(description);
+    });
+ });     
+}
+
+function getFirstLetter(){
+        var word = $('#term').val();
+        if(word.length > 0){
+          var firstLetter = word.charAt(0);    
+          return firstLetter;
+        }else{
+            return "";
+        }
 }
