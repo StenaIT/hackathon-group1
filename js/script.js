eb6fee65b5e3dae7e9e7c31a6478259f5f9ba210
Anton Karlsson <anton.karlsson@stena.com>
2016-09-15 16:42:53 +0200

    More functions to be able to translate words.

--- js/script.js
+++ js/script.js
@@ -25,6 +25,62 @@ function getText(){
  });     
 }
 
+function clearWords(){
+    $('#list').remove();
+	window.location.reload();
+}
+
+function traslateWords(){
+    $(document).ready(function() {
+        var url = "http://verksamtproxy.hackastack.de18292d.svc.dockerapp.io/";
+        var dict = [];
+        var letters = ['A','B','C','F','D','E','F','G','H','I','J','K','L','M','N','O','P','R','S','T','U','V','Y','Å','Ä','Ö'];
+        $.getJSON(url, function(data){
+            
+            var setence = $('#term').val();
+            var words = setence.split(" ");
+            words.forEach(function(theWord) {
+                var word = clearDot(theWord)
+                var first = getFirstLetterByWord(word).toUpperCase();
+                var indexOf = letters.indexOf(first);
+                if(indexOf >= 0 && indexOf < letters.length){
+                var allWords = data[indexOf].words;
+                allWords.forEach(function(element) {
+                  if(word.toUpperCase() == element.word.toUpperCase()){
+                    dict.push({
+                        key:   element.word,
+                        value: element.description
+                    });    
+                  }
+                }, this);
+                }   
+            }, this);
+            
+            var text = "<ul class='list'>";
+            for (var key in dict) {
+                text += "<li>" + dict[key].key + " => " + dict[key].value + "</li>";
+            }
+            text += "</ul>"
+            $('div.translated').html(text);
+        });
+
+        
+    });
+}
+
+function clearDot(word){
+    return word.replace('.','');
+}
+
+function getFirstLetterByWord(word){
+        if(word.length > 0){
+          var firstLetter = word.charAt(0);    
+          return firstLetter;
+        }else{
+            return "";
+        }
+}
+
 function getFirstLetter(){
         var word = $('#term').val();
         if(word.length > 0){
